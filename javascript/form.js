d919efd90456021cfa8c2ec8699604acf6284b27
hawariotm <hawariotm@gmail.com>
2015-12-15 17:09:59 +0530

    login page

--- javascript/form.js
+++ javascript/form.js
@@ -1,25 +1,29 @@
 <!--validation form-->
 function validateForm() {
+    var message   = document.getElementById("message");
     var x = document.forms["myForm"]["first_name"].value;
     if (x == null || x == "") {
-        alert("fName must be filled out");
+        // alert("fName must be filled out");
+        message.innerHTML = "FName must be filled out";
         return false;
     }
      var y = document.forms["myForm"]["last_name"].value;
      if (y == null || y == "") {
-        alert("LName must be filled out");
+        // alert("LName must be filled out");
+        message.innerHTML = "LName must be filled out";
         return false;
     }
    var gender=document.myForm.sex.value;
     if ( (document.myForm.sex[0].checked == false ) && ( document.myForm.sex[1].checked == false ) )
     {
-    alert( "Please choose your Gender: Male or Female" );
+    // alert( "Please choose your Gender: Male or Female" );
+    message.innerHTML = "Please choose your Gender: Male or Female";
     return false;
     }
        var countryName=document.myForm.country.value;
     if(countryName == "Default")  
     {  
-    alert('Select your country from the list');  
+    message.innerHTML = "Please choose your country"; 
     return false;  
     }
 
@@ -27,24 +31,24 @@ function validateForm() {
     var language=document.myForm.language.value;
      if ( (document.myForm.language[0].checked == false ) && ( document.myForm.language[1].checked == false ) )
     {
-    alert( "Please choose your language" );
+    message.innerHTML = "Please choose your language";
     return false;
     }
      var z = document.forms["myForm"]["user_id"].value;
      if (z == null || z == "") {
-        alert("user id must be filled out");
+       message.innerHTML = "userid must be filled out";
         return false;
     }
 
     var a = document.forms["myForm"]["password"].value;
      if (a == null || a == "") {
-        alert("password must be filled out");
+        message.innerHTML = "password must be filled out";
         return false;
     }
      
      var b = document.forms["myForm"]["email"].value;
      if (b == null || b == "") {
-        alert("email must be filled out");
+        message.innerHTML = "retype email";
         return false;
     }
 alert("Registration successfull");
